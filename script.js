8a386822e5cc208e2094346598ec1e49b969d1f8
Tramio <tramiodev@outlook.fr>
2021-08-22 16:43:09 +0200

    Create add, substract, multiply and divide functions

---
+++ script.js
@@ -0,0 +1,19 @@
+function add(a, b) {
+    let sum = a + b;
+    return sum;
+}
+
+function substract(a, b) {
+    let difference = a - b;
+    return difference;
+}
+
+function multiply(a, b) {
+    let product = a * b;
+    return product;
+}
+
+function divide(a, b) {
+    let quotient = a / b;
+    return quotient;
+}
